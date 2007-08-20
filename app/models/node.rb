1ee1fb19552a344654066c612e48bb6e41b40da7
Martin Eriksen <marberi@stud.ntnu.no>
2007-08-20 17:47:04 +0200

    uuid should be unique

--- app/models/node.rb
+++ app/models/node.rb
@@ -22,6 +22,7 @@ class Node < ActiveRecord::Base
   has_and_belongs_to_many :tags
   
   validates_presence_of :uuid
+  validates_uniqueness_of :uuid
   
   validates_format_of :uuid,
                       :with    => UUID_REGEX,
