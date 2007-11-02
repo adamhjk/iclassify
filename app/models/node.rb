6de3d8516b709b208a499ae5e8df8033d24e6130
Adam Jacob <adam@hjksolutions.com>
2007-11-02 11:30:56 -0700

    more mucking

--- app/models/node.rb
+++ app/models/node.rb
@@ -30,7 +30,7 @@ class Node < ActiveRecord::Base
                       
   # acts_as_ferret(:fields => [ :uuid, :notes, :description, :tag ], :remote =>  true )
   
-  acts_as_solr(:fields => [ :uuid, :notes, :description, :tag ], 
+  acts_as_solr(:fields => [ {:uuid => :string}, {:notes => :string}, {:description => :string}, {:tag => :string} ], 
                :auto_commit => true)
   
   # FIXME: Acts as tree needs to be added.
@@ -78,7 +78,7 @@ class Node < ActiveRecord::Base
     if evaluate_condition(configuration[:if], self) 
       logger.debug "solr_save: #{self.class.name} : #{record_id(self)}"
       this_doc = to_solr_doc
-      field_type = configuration[:facets] && configuration[:facets].include?(field) ? :facet : :text
+      field_type = configuration[:facets] && configuration[:facets].include?(field) ? :facet : :string
       field_boost= solr_configuration[:default_boost]
       suffix = get_solr_field_type(field_type)
       attribs.each do |attrib|
@@ -98,6 +98,37 @@ class Node < ActiveRecord::Base
     else
       solr_destroy
     end
+  end
+  
+  def check_solr_string(v)
+    if v =~ /(\.|\_|\:|\*|\(|\)|\-|\=)/
+      get_solr_field_type(:string)
+    else
+      get_solr_field_type(:text)
+    end
+  end
+  
+  # Replaces the field types based on the types (if any) specified
+  # on the acts_as_solr call
+  def replace_types(strings, include_colon=true)
+    suffix = include_colon ? ":" : ""
+    if configuration[:solr_fields] && configuration[:solr_fields].is_a?(Array)
+      configuration[:solr_fields].each do |solr_field|
+        field_type = get_solr_field_type(:string)
+        if solr_field.is_a?(Hash)
+          solr_field.each do |name,value|
+       	    if value.respond_to?(:each_pair)
+              field_type = get_solr_field_type(value[:type]) if value[:type]
+            else
+              field_type = get_solr_field_type(value)
+            end
+            field = "#{name.to_s}_#{field_type}#{suffix}"
+            strings.each_with_index { |s,i| strings[i] = s.gsub(/#{name.to_s}_s#{suffix}/,field) }
+          end
+        end
+      end
+    end
+    strings
   end
   
   def tag
@@ -179,6 +210,53 @@ class Node < ActiveRecord::Base
       self.attribs = Attrib.create_missing_attribs(self, attrib_array, { :delete => true })
     end
     self.save
+  end
+  
+  def parse_query(query=nil, options={}, models=nil)
+    valid_options = [:offset, :limit, :facets, :models, :results_format, :order, :scores, :operator]
+    query_options = {}
+    return if query.nil?
+    raise "Invalid parameters: #{(options.keys - valid_options).join(',')}" unless (options.keys - valid_options).empty?
+    begin
+      Deprecation.validate_query(options)
+      query_options[:start] = options[:offset]
+      query_options[:rows] = options[:limit]
+      query_options[:operator] = options[:operator]
+      
+      # first steps on the facet parameter processing
+      if options[:facets]
+        query_options[:facets] = {}
+        query_options[:facets][:limit] = -1  # TODO: make this configurable
+        query_options[:facets][:sort] = :count if options[:facets][:sort]
+        query_options[:facets][:mincount] = 0
+        query_options[:facets][:mincount] = 1 if options[:facets][:zeros] == false
+        query_options[:facets][:fields] = options[:facets][:fields].collect{|k| "#{k}_facet"} if options[:facets][:fields]
+        query_options[:filter_queries] = replace_types(options[:facets][:browse].collect{|k| "#{k.sub!(/ *: */,"_facet:")}"}) if options[:facets][:browse]
+        query_options[:facets][:queries] = replace_types(options[:facets][:query].collect{|k| "#{k.sub!(/ *: */,"_t:")}"}) if options[:facets][:query]
+      end
+      
+      if models.nil?
+        # TODO: use a filter query for type, allowing Solr to cache it individually
+        models = "AND #{solr_configuration[:type_field]}:#{self.name}"
+        field_list = solr_configuration[:primary_key_field]
+      else
+        field_list = "id"
+      end
+      
+      query_options[:field_list] = [field_list, 'score']
+      query = "(#{query.gsub(/ *: */,"_s:")}) #{models}"
+      order = options[:order].split(/\s*,\s*/).collect{|e| e.gsub(/\s+/,'_t ').gsub(/\bscore_t\b/, 'score')  }.join(',') if options[:order] 
+      query_options[:query] = replace_types([query])[0] # TODO adjust replace_types to work with String or Array  
+
+      if options[:order]
+        # TODO: set the sort parameter instead of the old ;order. style.
+        query_options[:query] << ';' << replace_types([order], false)[0]
+      end
+             
+      ActsAsSolr::Post.execute(Solr::Request::Standard.new(query_options))
+    rescue
+      raise "There was a problem executing your search: #{$!}"
+    end            
   end
   
   # Serializes all the nodes in the database                
