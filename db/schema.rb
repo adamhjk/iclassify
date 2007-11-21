9b33d1afb4105ec42bc8a1b6f15bda3249fb6c6c
Adam Jacob <adam@hjksolutions.com>
2007-11-20 23:32:40 -0800

    Adding node upgrade migration, switching to https default

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 10) do
+ActiveRecord::Schema.define(:version => 11) do
 
   create_table "attribs", :force => true do |t|
     t.column "node_id", :integer, :null => false
