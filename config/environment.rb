2135d4f077697c7e01ef2b48c65f9c56269fd25d
Adam Jacob <adam@hjksolutions.com>
2007-12-09 16:44:59 -0800

    Upgraded to rails 2.0

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.6' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,6 +15,8 @@ Rails::Initializer.run do |config|
   
   # Skip frameworks you're not going to use (only works if using vendor/rails)
   config.frameworks -= [ :action_web_service, :action_mailer ]
+  
+  config.action_controller.session = { :session_key => "iclassify_session", :secret => "mkn1234lkjasdlfhvnzxdfalw34kj1239087asdfj!nsdf$%@!fnlkasdfjl!" }
 
   # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
   # config.plugins = %W( exception_notification ssl_requirement )
