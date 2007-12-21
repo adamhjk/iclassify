515ea2420215e8a134dabfbb27ea457f96afa0a6
Adam Jacob <adam@hjksolutions.com>
2007-12-21 01:25:54 -0800

    need to set the whole thing explicitly

--- config/environment.rb
+++ config/environment.rb
@@ -61,7 +61,7 @@ end
 # Mime::Type.register "application/x-mobile", :mobile
 # Include your application configuration below
 
-ActionController::ACCEPTED_HTTP_METHODS << "options"
+ActionController::ACCEPTED_HTTP_METHODS = Set.new(%w( get head put post delete options ))
 
 #module ActionController
 #  # HTTP methods which are accepted by default. 
